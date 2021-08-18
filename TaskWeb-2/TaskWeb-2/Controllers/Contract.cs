2516d1564585befbcfb5a1a5fb6d020e6a9ff138
Сергей <gan7er@yandex.ru>
2021-08-18 20:50:02 +0300

    add controller

---
+++ TaskWeb-2/TaskWeb-2/Controllers/Contract.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskWeb_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Contract : ControllerBase
+    {
+    }
+}
